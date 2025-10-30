8be0f1e0d58f900c11289d4dd86ba369033e3250
senzenn <ascenzen11@gmail.com>
2025-10-30 16:27:42 +0530

    changed name

--- src/components/sections/navbar.tsx
+++ src/components/sections/navbar.tsx
@@ -136,7 +136,7 @@ export function Navbar() {
             {/* LOGO */}
             <Link href="/" className="flex items-center gap-3 shrink-0">
               <Icons.logo className="size-7 md:size-10" />
-              <p className="text-lg font-semibold text-primary whitespace-nowrap">SkyAgent</p>
+              <p className="text-lg font-semibold text-primary whitespace-nowrap">Sky Pay</p>
             </Link>
 
             <NavMenu />
